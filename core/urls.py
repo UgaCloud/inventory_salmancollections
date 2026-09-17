6d3c51697085129ae7bf7728ce2758a5f4aaaf28
Foren <osintforen64@gmail.com>
2026-09-17 21:49:18 +0300

    Refactor urlpatterns to include Debug Toolbar
    Enable Debug Toolbar conditionally based on DEBUG setting.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,28 @@
+# from django.contrib import admin
+# from django.urls import path, include
+# from debug_toolbar.toolbar import debug_toolbar_urls
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('app.urls')),
+#     path('api/', include('api.urls')),
+# ] + debug_toolbar_urls()
+
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('api/', include('api.urls')),
-] + debug_toolbar_urls()
+]
+
+# Only enable Debug Toolbar locally when DEBUG is True
+if settings.DEBUG:
+    try:
+        from debug_toolbar.toolbar import debug_toolbar_urls
+        urlpatterns += debug_toolbar_urls()
+    except ImportError:
+        pass
